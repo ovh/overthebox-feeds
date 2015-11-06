8c9d9259d013624667fd6243c9caa3623423cb45
Simon Lelievre <simon.lelievre@corp.ovh.com>
2015-11-06 14:19:29 +0100

    keep foreground mwan3 restart

--- overthebox/files/usr/lib/lua/overthebox.lua
+++ overthebox/files/usr/lib/lua/overthebox.lua
@@ -134,7 +134,7 @@ function restart(service)
         return true, ret
 end
 function restartmwan3()
-        local ret = os.execute("nohup /usr/sbin/mwan3 restart &")
+        local ret = os.execute("/usr/sbin/mwan3 restart")
         return true, ret
 end
 
