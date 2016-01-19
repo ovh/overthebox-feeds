75fd616f797e079164ea4cdee59573681a40e0d2
angt <angt@users.noreply.github.com>
2016-01-19 16:52:33 +0100

    Update glorytun to version 0.0.25

--- glorytun/files/usr/sbin/glorytun.sh
+++ glorytun/files/usr/sbin/glorytun.sh
@@ -6,61 +6,54 @@ if [ -z "${dev}" -o -z "${iplocal}" -o -z "${ippeer}" ]; then
     exit 1
 fi
 
-STATE=/tmp/glorytun.fifo
-mkfifo $STATE
+statefile=/tmp/glorytun.fifo
+mkfifo ${statefile}
 
 trap "pkill -TERM -P $$" TERM
-echo glorytun dev ${dev} statefile $STATE retry count -1 $*  | logger -t glorytun
-glorytun dev ${dev} statefile $STATE retry count -1 timeout 5000 $* &
+logger -t glorytun RUN dev ${dev} statefile ${statefile} retry count -1 timeout 5000 $*
+glorytun dev ${dev} statefile ${statefile} retry count -1 timeout 5000 $* &
+
 GTPID=$!
 
 initialized() {
-    logger -t glorytun SETUP ${dev}
-
     ip addr add ${iplocal} peer ${ippeer} dev ${dev}
     [ -n "${mtu}" ] && ip link set ${dev} mtu ${mtu}
     ip link set ${dev} up
 
     multipath ${dev} off
 }
+
 started() {
-    logger -t glorytun UP
-    [ -z "${pref}" ] && pref=0
-
     if [ -n "${table}" ]; then
-	ip rule add from ${iplocal} table ${table} pref ${pref}
-	ip route add default via ${ippeer} table ${table}
-	[ -n "${metric}" ] && ip route add default via ${ippeer} metric ${metric}
+        ip rule add from ${iplocal} table ${table} pref ${pref:-0}
+        ip route add default via ${ippeer} table ${table}
+        [ -n "${metric}" ] && ip route add default via ${ippeer} metric ${metric}
     fi
 }
 
-down() {
-    logger -t glorytun STOP
-
+stopped() {
     if [ -n "${table}" ]; then
-	ip rule del from ${iplocal} table ${table}
-	ip route del default via ${ippeer} table ${table}
+        ip rule del from ${iplocal} table ${table}
+        ip route del default via ${ippeer} table ${table}
     fi
 }
 
-while true; do
-    kill -0 ${GTPID} || break
-    read line || break
-    case $line in
-	INITIALIZED)
-	    initialized
-	    ;;
-	STARTED)
-	    started
-	    ;;
-	STOPPED)
-	    down
-	    ;;
-	*)
-	    logger -t glorytun unknown : $line
-	    ;;
+while kill -0 ${GTPID}; do
+    read STATE INFO || break
+    logger -t glorytun ${STATE} ${INFO}
+    case ${STATE} in
+        INITIALIZED)
+            initialized
+            ;;
+        STARTED)
+            started
+            ;;
+        STOPPED)
+            stopped
+            ;;
     esac
-done < $STATE
+done < ${statefile}
 
-down
+stopped
+
 logger -t glorytun BYE
