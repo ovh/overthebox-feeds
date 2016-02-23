21b8868959c822a332e812b6013a366fe8391e58
Sebastien DUPONCHEEL <sebastien.duponcheel@ovh.net>
2016-02-23 13:59:46 +0100

    xtun0: fixed xtun0 not included in firewall config

--- overthebox/files/usr/lib/lua/overthebox.lua
+++ overthebox/files/usr/lib/lua/overthebox.lua
@@ -165,6 +165,7 @@ function config()
 		end
 		uci:save('network')
 		uci:commit('network')
+		
 		uci:save('vtund')
 		uci:commit('vtund')
 		table.insert(ret, "vtund")
@@ -1045,6 +1046,8 @@ function update_confmwan()
 	uci:commit("mwan3")
 	-- Saving net conf md5 and restarting services
 	if os.execute("mwan3 status 1>/dev/null 2>/dev/null") == 0 then
+		os.execute("nohup /etc/init.d/network reload &")
+		os.execute("nohup /etc/init.d/firewall reload &")
 		os.execute("nohup /usr/sbin/mwan3 restart &")
 		os.execute("nohup /etc/init.d/vtund restart &")
 	end
