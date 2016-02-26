58f0b866202c711f3556d493b7befe7b7a1ce1db
Sebastien DUPONCHEEL <sebastien.duponcheel@ovh.net>
2016-02-26 10:49:17 +0100

    Vtun: apply vtun mtu values

--- vtund/files/vtund.ifup.sh
+++ vtund/files/vtund.ifup.sh
@@ -5,8 +5,10 @@ remoteip=$3
 metric=$4
 table=$5
 pref=$6
+mtu=$7
 
 /sbin/ifconfig $dev $localip pointopoint $remoteip up;
+/sbin/ifconfig $dev mtu $mtu;
 /usr/bin/multipath $dev off;
 /usr/sbin/ip rule add from $localip table $table pref $pref;
 /usr/sbin/ip route add default via $remoteip table $table;
