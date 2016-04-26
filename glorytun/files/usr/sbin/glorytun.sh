fe53a7938054d0cdc4e18cdb66330ba57a9d5cd2
angt <angt@users.noreply.github.com>
2016-04-26 07:03:17 +0000

    Update glorytun to 0.0.30

--- glorytun/files/usr/sbin/glorytun.sh
+++ glorytun/files/usr/sbin/glorytun.sh
@@ -1,68 +1,98 @@
 #!/bin/sh
 
-if [ -z "${dev}" -o -z "${iplocal}" -o -z "${ippeer}" ]; then
+source /lib/functions.sh
+
+if [ -z "${GLORYTUN_DEV}" -o -z "${GLORYTUN_IP_LOCAL}" -o -z "${GLORYTUN_IP_PEER}" -o -z "${GLORYTUN_HOST}" -o -z "${GLORYTUN_PORT}" ]; then
     echo "environnement variable is not set"
-    echo "run this binary with procd"
     exit 1
 fi
 
-statefile=/tmp/glorytun.${dev}.fifo
-[ -e "${statefile}" ] && rm -f "${statefile}"
-mkfifo ${statefile}
+statefile=/tmp/glorytun.${GLORYTUN_DEV}.fifo
+
+rm -f "${statefile}"
+mkfifo "${statefile}"
+
+if glorytun version | grep mud ; then
+    GLORYTUN_ARGS="bind-port ${GLORYTUN_PORT} bind "
+
+    add_multipath () {
+        config_get ifname $1 ifname
+        config_get multipath $1 multipath
+        [ "${multipath}" = "on" ] && GLORYTUN_ARGS="${GLORYTUN_ARGS}${ifname},"
+    }
+
+    config_load network
+    config_foreach add_multipath interface
+else
+    GLORYTUN_ARGS="retry count -1 const 5000000 timeout 5000 keepalive count 3 idle 10 interval 1 mptcp"
+fi
 
 trap "pkill -TERM -P $$" TERM
-logger -t glorytun RUN dev ${dev} statefile ${statefile} retry count -1 timeout 5000 $*
-glorytun dev ${dev} statefile ${statefile} retry count -1 const 5000000 timeout 5000 keepalive count 3 idle 10 interval 1 $* &
-
+glorytun host ${GLORYTUN_HOST} port ${GLORYTUN_PORT} dev ${GLORYTUN_DEV} statefile ${statefile} ${GLORYTUN_ARGS} $* &
 GTPID=$!
 
 initialized() {
-    ip addr add ${iplocal} peer ${ippeer} dev ${dev}
-    [ -n "${mtu}" ] && ip link set ${dev} mtu ${mtu}
+    ip addr add ${GLORYTUN_IP_LOCAL} peer ${GLORYTUN_IP_PEER} dev ${GLORYTUN_DEV}
+    [ -n "${GLORYTUN_MTU}" ] && ip link set ${GLORYTUN_DEV} mtu ${GLORYTUN_MTU}
+    [ -n "${GLORYTUN_TXQLEN}" ] && ip link set ${GLORYTUN_DEV} txqueuelen ${GLORYTUN_TXQLEN}
+
     # Workaround to make mwan3 update tun rule
-    /etc/init.d/sqm start ${dev}
-    multipath ${dev} off
+    /etc/init.d/sqm start ${GLORYTUN_DEV}
+
+    multipath ${GLORYTUN_DEV} off
 }
 
 started() {
-    ip link set ${dev} up
-    if [ -n "${table}" ]; then
-        ip rule add from ${iplocal} table ${table} pref ${pref:-0}
-        ip route add default via ${ippeer} table ${table}
+    ip link set ${GLORYTUN_DEV} up
+
+    if [ -n "${GLORYTUN_TABLE}" ]; then
+        ip rule add from ${GLORYTUN_IP_LOCAL} table ${GLORYTUN_TABLE} pref ${GLORYTUN_PREF}
+        ip route add default via ${GLORYTUN_IP_PEER} table ${GLORYTUN_TABLE}
     fi
-    [ -n "${metric}" ] && ip route add default via ${ippeer} metric ${metric}
-    ubus call network.interface.${dev} up
-    if [ "${dev}" == "tun0" ]; then
-        [ -x /etc/init.d/shadowsocks ] && /etc/init.d/shadowsocks start ;
+
+    if [ -n "${GLORYTUN_METRIC}" ]; then
+        ip route add default via ${GLORYTUN_IP_PEER} metric ${GLORYTUN_METRIC}
+    fi
+
+    ubus call network.interface.${GLORYTUN_DEV} up
+
+    if [ "${GLORYTUN_DEV}" == "tun0" ]; then
+        [ -x /etc/init.d/shadowsocks ] && /etc/init.d/shadowsocks start;
     fi
 }
 
 stopped() {
-    if [ "${dev}" == "tun0" ]; then
-    	[ -x /etc/init.d/shadowsocks ] && /etc/init.d/shadowsocks stop;
+    if [ "${GLORYTUN_DEV}" == "tun0" ]; then
+        [ -x /etc/init.d/shadowsocks ] && /etc/init.d/shadowsocks stop;
     fi
-    if [ -n "${table}" ]; then
-        ip rule del from ${iplocal} table ${table}
-        ip route del default via ${ippeer} table ${table}
-	ip route del default via ${ippeer} metric ${metric}
+
+    if [ -n "${GLORYTUN_TABLE}" ]; then
+        ip rule del from ${GLORYTUN_IP_LOCAL} table ${GLORYTUN_TABLE}
+        ip route del default via ${GLORYTUN_IP_PEER} table ${GLORYTUN_TABLE}
     fi
-    ubus call network.interface.${dev} down
-    ip link set ${dev} down
+
+    if [ -n "${GLORYTUN_METRIC}" ]; then
+        ip route del default via ${GLORYTUN_IP_PEER} metric ${GLORYTUN_METRIC}
+    fi
+
+    ubus call network.interface.${GLORYTUN_DEV} down
+
+    ip link set ${GLORYTUN_DEV} down
 }
 
 while kill -0 ${GTPID}; do
     read STATE INFO || break
     logger -t glorytun ${STATE} ${INFO}
     case ${STATE} in
-        INITIALIZED)
-            initialized
-            ;;
-        STARTED)
-            started
-            ;;
-        STOPPED)
-            stopped
-            ;;
+    INITIALIZED)
+        initialized
+        ;;
+    STARTED)
+        started
+        ;;
+    STOPPED)
+        stopped
+        ;;
     esac
 done < ${statefile}
 
