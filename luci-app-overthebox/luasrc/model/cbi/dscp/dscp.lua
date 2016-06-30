933e946fc14437188831a1d57513912e88d532c1
Sebastien DUPONCHEEL <sebastien.duponcheel@ovh.net>
2016-06-30 14:20:09 +0200

    Enable Customizable QoS download

--- luci-app-overthebox/luasrc/model/cbi/dscp/dscp.lua
+++ luci-app-overthebox/luasrc/model/cbi/dscp/dscp.lua
@@ -15,50 +15,68 @@ s.addremove = true
 s.sortable  = true
 
 function cbiAddProtocol(field)
-        local protocols = ut.trim(sys.exec("cat /etc/protocols | grep '\\s# ' | awk '{print $1}' | grep -v '^#' | grep -vw -e 'ip' -e 'tcp' -e 'udp' -e 'icmp' -e 'esp' | grep -v 'ipv6' | sort | tr '\n' ' '"))
-        for p in string.gmatch(protocols, "%S+") do
-                field:value(p)
-        end
+	local protocols = ut.trim(sys.exec("cat /etc/protocols | grep '\\s# ' | awk '{print $1}' | grep -v '^#' | grep -vw -e 'ip' -e 'tcp' -e 'udp' -e 'icmp' -e 'esp' | grep -v 'ipv6' | sort | tr '\n' ' '"))
+	for p in string.gmatch(protocols, "%S+") do
+		field:value(p)
+	end
 end
 
+direction = s:option(ListValue, "direction", translate("Direction"))
+	direction.default = "upload"
+	direction.rmempty = false
+	direction:value("upload")
+	direction:value("download")
+	direction:value("both")
+
+--dpi = s:option(Value, "dpi", translate("Service"))
+--dpi.rmempty = true
+--dpi:depends("direction", "download")
+--dpi:value("", translate("Disabled"))
+
 proto = s:option(Value, "proto", translate("Protocol"))
-        proto.default = "all"
-        proto.rmempty = false
-        proto:value("tcp")
-        proto:value("udp")
-        proto:value("all")
-        proto:value("ip")
-        proto:value("icmp")
-        proto:value("esp")
-        cbiAddProtocol(proto)
+	proto:depends("dpi", "")
+	proto.default = "all"
+	proto.rmempty = false
+	proto:value("tcp")
+	proto:value("udp")
+	proto:value("all")
+	proto:value("ip")
+	proto:value("icmp")
+	proto:value("esp")
+	cbiAddProtocol(proto)
 
 srch = s:option(Value, "src_ip", translate("Source host"))
-srch.rmempty = true
-srch:value("", translate("all"))
-wa.cbi_add_knownips(srch)
+	srch:depends("dpi", "")
+	srch.rmempty = true
+	srch:value("", translate("all"))
+	wa.cbi_add_knownips(srch)
 
-ports = s:option(Value, "src_port", translate("Source ports"))
-ports.rmempty = true
-ports:value("", translate("all"))
+sports = s:option(Value, "src_port", translate("Source ports"))
+	sports:depends("dpi", "")
+	sports.rmempty = true
+	sports:value("", translate("all"))
 
 dsth = s:option(Value, "dest_ip", translate("Destination host"))
-dsth.rmempty = true
-dsth:value("", translate("all"))
-wa.cbi_add_knownips(dsth)
+	dsth.rmempty = true
+	dsth:value("", translate("all"))
+	dsth:depends("direction", "upload")
+	wa.cbi_add_knownips(dsth)
 
-ports = s:option(Value, "dest_port", translate("Destination ports"))
-ports.rmempty = true
-ports:value("", translate("all"))
+dports = s:option(Value, "dest_port", translate("Destination ports"))
+	dports.rmempty = true
+	dports:value("", translate("all"))
+	dports:depends("direction", "upload")
 
 t = s:option(ListValue, "class", translate("Class"))
-t:value("cs1", translate("CS1 - Scavenger"))
-t:value("cs2", translate("CS2 - Normal"))
-t:value("cs3", translate("CS3 - Signaling"))
-t:value("cs4", translate("CS4 - Realtime"))
-t:value("cs5", translate("CS5 - Broadcast video"))
-t:value("cs6", translate("CS6 - Network control"))
-t:value("cs7", translate("CS7 - Reserved"))
-t.default = "cs2"
+	t:value("cs1", translate("CS1 - Scavenger"))
+	t:value("cs2", translate("CS2 - Normal"))
+	t:value("cs3", translate("CS3 - Signaling"))
+	t:value("cs4", translate("CS4 - Realtime"))
+	t:value("cs5", translate("CS5 - Broadcast video"))
+	t:value("cs6", translate("CS6 - Network control"))
+	t:value("cs7", translate("CS7 - Reserved"))
+	--t:value("drop", translate("Drop"))
+	t.default = "cs2"
 
 comment = s:option(Value, "comment", translate("Comment"))
 --bytes = s:option(Value, "connbytes", translate("Number of bytes"))
