a0ec6a82280e21ade1192646b480080d38a4b9d3
Lucas BEE <pouulet@gmail.com>
2016-09-20 14:28:24 +0200

    Bugfix/check config return (#154)
    * Delete trailing spaces in overthebox.lua
    
    * Add check of the return of GET on /config
    
    (cherry picked from commit 0cbf76e80e59ac5a44e2883bc8a3bed74d54e0ed)
    
    Conflicts:
    	overthebox/files/usr/lib/lua/overthebox.lua

--- overthebox/files/usr/lib/lua/overthebox.lua
+++ overthebox/files/usr/lib/lua/overthebox.lua
@@ -118,9 +118,13 @@ function addInterfaceInZone(name, ifname)
 end
 
 function config()
+	local ret = {}
 	local uci = uci.cursor()
 	local rcode, res = GET('devices/'..uci:get("overthebox", "me", "device_id", {}).."/config")
-	local ret = {}
+	if rcode ~= 200 then
+		table.insert(ret, "Error getting config : ".. rcode)
+		return false, ret
+	end
 
 	if res.glorytun_conf and exists( res.glorytun_conf, 'server', 'port', 'key', 'dev', 'ip_peer', 'ip_local', 'mtu' ) then
 		uci:set('glorytun', 'otb', 'tunnel')
@@ -234,7 +238,7 @@ function config()
 	end
 
 	if res.tun_conf.app == 'glorytun_mud' then
-		-- Activate MUD 
+		-- Activate MUD
 		uci:foreach("glorytun", "mud",
 			function (e)
 				uci:set('glorytun', e[".name"], 'enable', '1')
@@ -332,7 +336,7 @@ function config()
 		table.insert(ret, 'log')
 	end
 
-	return true, ret 
+	return true, ret
 end
 
 function send_properties( props )
@@ -491,7 +495,7 @@ end
 
 function run_diagnostic( id, name, arg )
 	cmd = string.gsub( diags[name].cmd, "{{(%w+)}}", function(w)
-		return (arg and arg[w]) or diags[name].default[w] or "" 
+		return (arg and arg[w]) or diags[name].default[w] or ""
 	end )
 	local ret, rcode = run(cmd)
 	local ret_api = post_result_diagnostic(id, name, cmd, ret, rcode)
@@ -935,7 +939,7 @@ function API(uri, method, data)
 		method = method,
 		url = url,
 		protocol = "tlsv1",
-		headers = 
+		headers =
 		{
 			["Content-Type"] = "application/json",
 			["Content-length"] = reqbody:len(),
@@ -1042,7 +1046,7 @@ function update_confmwan()
 	table.insert( tracking_servers, "51.254.49.133" )
 	local tracking_tunnels = {}
 	table.insert( tracking_tunnels, "169.254.254.1" )
-	-- 
+	--
 	local interfaces={}
 	local size_interfaces = 0 -- table.getn( does not work....
 
@@ -1141,7 +1145,7 @@ function update_confmwan()
 		for key in pairs(t) do
 			table.insert( orderedIndex, key )
 		end
-		table.sort( orderedIndex, function (a, b) 
+		table.sort( orderedIndex, function (a, b)
 			return tonumber(interfaces[a].metric or 0) < tonumber(interfaces[b].metric or 0)
 		end )
 		return orderedIndex
@@ -1317,11 +1321,11 @@ function update_confmwan()
 	end
 
 	function table_copy(obj, seen)
-		if type(obj) ~= 'table' then 
-			return obj 
+		if type(obj) ~= 'table' then
+			return obj
 		end
-		if seen and seen[obj] then 
-			return seen[obj] 
+		if seen and seen[obj] then
+			return seen[obj]
 		end
 		local s = seen or {}
 		local res = setmetatable({}, getmetatable(obj))
@@ -1356,7 +1360,7 @@ function update_confmwan()
 
 	-- Setting rule to forward all non tcp traffic to tun0
 	if not uci:get("mwan3", "all") then
-		uci:set("mwan3", "all", "rule") 
+		uci:set("mwan3", "all", "rule")
 		uci:set("mwan3", "all", "proto", "all")
 		uci:set("mwan3", "all", "sticky", "0")
 	end
@@ -1649,7 +1653,7 @@ function create_dhcp_server()
 	-- Setup a dhcp server if needed
 	local dhcpd_configured = 0
 	local dhcpd = list_running_dhcp()
-	for i, _ in pairs(dhcpd) do    
+	for i, _ in pairs(dhcpd) do
 		dhcpd_configured = dhcpd_configured + 1
 	end
 	log( "Count of dhcp configured : " .. dhcpd_configured )
@@ -1758,7 +1762,7 @@ end
 
 function restart_daemon()
 	local ret, rcode = run("/etc/init.d/overtheboxd restart")
-	return status_code_ok(rcode), ret 
+	return status_code_ok(rcode), ret
 end
 
 
@@ -1959,7 +1963,7 @@ function iface_info(iface)
 			end
 		end
 	end
-	
+
 	return result
 end
 
@@ -2000,7 +2004,7 @@ function tc_stats()
 	result["download"] = {}
 	local json = json.decode(sys.exec("curl -s --max-time 1 api/qos/tcstats"))
 	if json and json.raw_output then
-		
+
 		for line in string.gmatch(json.raw_output, '[^\r\n]+') do
 			table.insert(output, line)
 		end
@@ -2082,6 +2086,3 @@ end
 function err(msg)
 	posix.syslog( posix.LOG_ERR, msg)
 end
-
-
-
