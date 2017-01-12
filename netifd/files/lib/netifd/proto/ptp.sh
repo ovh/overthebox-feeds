de0522b6f4c50fe4592523789c53b75f8d1eaafe
Sebastien DUPONCHEEL <sebastien.duponcheel@ovh.net>
2017-01-12 17:46:25 +0100

    netifd: fixed loop when glorytun goes down and remove txqueuelen workaround

--- netifd/files/lib/netifd/proto/ptp.sh
+++ netifd/files/lib/netifd/proto/ptp.sh
@@ -7,30 +7,19 @@ init_proto "$@"
 proto_ptp_init_config() {
 	proto_config_add_string 'ipaddr:ipaddr'
 	proto_config_add_string 'gateway:gateway'
-	proto_config_add_int 'txqueuelen:txqueuelen'
-
-	no_device=1
-	available=1
 }
 
 proto_ptp_setup() {
 	local interface="$1"; shift
 
-	json_select ipv4
+	json_select
 	json_get_vars ipaddr gateway
 
 	proto_init_update "$interface" 1
 	proto_set_keep 1
 	proto_add_ipv4_address "$ipaddr" "255.255.255.255" "" "$gateway"
 	proto_add_ipv4_route "0.0.0.0" 0 "$gateway"
-	# txqueuelen is not correctly handled by device layer
-	/usr/bin/ip link set $interface txqueuelen "$txqueuelen"
 	proto_send_update "$interface"
-}
-
-proto_ptp_teardown() {
-	local interface="$1"
-	proto_kill_command "$interface"
 }
 
 add_protocol ptp
