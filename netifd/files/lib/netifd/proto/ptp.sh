991d6097ea5ffc5c601ac66e5189d2d01968ae49
DUPONCHEEL Sébastien <sebastien.duponcheel@ovh.net>
2017-01-13 09:12:33 +0100

    netifd: fix ptp ipv4 warning, tear_down loop (#263)

--- netifd/files/lib/netifd/proto/ptp.sh
+++ netifd/files/lib/netifd/proto/ptp.sh
@@ -7,16 +7,12 @@ init_proto "$@"
 proto_ptp_init_config() {
 	proto_config_add_string 'ipaddr:ipaddr'
 	proto_config_add_string 'gateway:gateway'
-
-	no_device=1
-	available=1
 }
 
 proto_ptp_setup() {
 	local interface="$1"; shift
-	ipv4=1
 
-	json_select ipv4
+	json_select
 	json_get_vars ipaddr gateway
 
 	proto_init_update "$interface" 1
@@ -24,10 +20,6 @@ proto_ptp_setup() {
 	proto_add_ipv4_address "$ipaddr" "255.255.255.255" "" "$gateway"
 	proto_add_ipv4_route "0.0.0.0" 0 "$gateway"
 	proto_send_update "$interface"
-}
-
-proto_ptp_teardown() {
-	local interface="$1"
 }
 
 add_protocol ptp
