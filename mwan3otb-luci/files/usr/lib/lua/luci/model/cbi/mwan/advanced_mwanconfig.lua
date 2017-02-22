a73910b1557bd959a564d756f7cc1f9ac6cb6b15
Lucas BEE <pouulet@gmail.com>
2017-02-22 13:55:41 +0100

    mwan3otb-luci: Retab (#315)

--- mwan3otb-luci/files/usr/lib/lua/luci/model/cbi/mwan/advanced_mwanconfig.lua
+++ mwan3otb-luci/files/usr/lib/lua/luci/model/cbi/mwan/advanced_mwanconfig.lua
@@ -1,3 +1,4 @@
+-- vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 :
 -- ------ mwan configuration ------ --
 
 ut = require "luci.util"
@@ -6,27 +7,27 @@ mwanConfig = "/etc/config/mwan3"
 
 
 m5 = SimpleForm("luci", nil)
-	m5:append(Template("mwan/advanced_mwanconfig")) -- highlight current tab
+  m5:append(Template("mwan/advanced_mwanconfig")) -- highlight current tab
 
 
 f = m5:section(SimpleSection, nil,
-	translate("This section allows you to modify the contents of /etc/config/mwan3"))
+  translate("This section allows you to modify the contents of /etc/config/mwan3"))
 
 t = f:option(TextValue, "lines")
-	t.rmempty = true
-	t.rows = 20
+t.rmempty = true
+t.rows = 20
 
-	function t.cfgvalue()
-		return nixio.fs.readfile(mwanConfig) or ""
-	end
+function t.cfgvalue()
+  return nixio.fs.readfile(mwanConfig) or ""
+end
 
-	function t.write(self, section, data) -- format and write new data to script
-		return nixio.fs.writefile(mwanConfig, "\n" .. ut.trim(data:gsub("\r\n", "\n")) .. "\n")
-	end
+function t.write(self, section, data) -- format and write new data to script
+  return nixio.fs.writefile(mwanConfig, "\n" .. ut.trim(data:gsub("\r\n", "\n")) .. "\n")
+end
 
-	function f.handle(self, state, data)
-		return true
-	end
+function f.handle(self, state, data)
+  return true
+end
 
 
 return m5
--- mwan3otb-luci/files/usr/lib/lua/luci/model/cbi/mwan/ruleconfig.lua
+++ mwan3otb-luci/files/usr/lib/lua/luci/model/cbi/mwan/ruleconfig.lua
@@ -1,37 +1,38 @@
+-- vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 :
 -- ------ extra functions ------ --
 
 function ruleCheck() -- determine if rule needs a protocol specified
-	local sourcePort = ut.trim(sys.exec("uci -q -p /var/state get mwan3." .. arg[1] .. ".src_port"))
-	local destPort = ut.trim(sys.exec("uci -q -p /var/state get mwan3." .. arg[1] .. ".dest_port"))
-	if sourcePort ~= "" or destPort ~= "" then -- ports configured
-		local protocol = ut.trim(sys.exec("uci -q -p /var/state get mwan3." .. arg[1] .. ".proto"))
-		if protocol == "" or protocol == "all" then -- no or improper protocol
-			error_protocol = 1
-		end
-	end
+  local sourcePort = ut.trim(sys.exec("uci -q -p /var/state get mwan3." .. arg[1] .. ".src_port"))
+  local destPort = ut.trim(sys.exec("uci -q -p /var/state get mwan3." .. arg[1] .. ".dest_port"))
+  if sourcePort ~= "" or destPort ~= "" then -- ports configured
+    local protocol = ut.trim(sys.exec("uci -q -p /var/state get mwan3." .. arg[1] .. ".proto"))
+    if protocol == "" or protocol == "all" then -- no or improper protocol
+      error_protocol = 1
+    end
+  end
 end
 
 function ruleWarn() -- display warning message at the top of the page
-	if error_protocol == 1 then
-		return "<font color=\"ff0000\"><strong>WARNING: this rule is incorrectly configured with no or improper protocol specified! Please configure a specific protocol!</strong></font>"
-	else
-		return ""
-	end
+  if error_protocol == 1 then
+    return "<font color=\"ff0000\"><strong>WARNING: this rule is incorrectly configured with no or improper protocol specified! Please configure a specific protocol!</strong></font>"
+  else
+    return ""
+  end
 end
 
 function cbiAddPolicy(field)
-	uci.cursor():foreach("mwan3", "policy",
-		function (section)
-			field:value(section[".name"])
-		end
-	)
+  uci.cursor():foreach("mwan3", "policy",
+    function (section)
+      field:value(section[".name"])
+    end
+  )
 end
 
 function cbiAddProtocol(field)
-	local protocols = ut.trim(sys.exec("cat /etc/protocols | grep '	# ' | awk '{print $1}' | grep -vw -e 'ip' -e 'tcp' -e 'udp' -e 'icmp' -e 'esp' | grep -v 'ipv6' | sort | tr '\n' ' '"))
-	for p in string.gmatch(protocols, "%S+") do
-		field:value(p)
-	end
+  local protocols = ut.trim(sys.exec("cat /etc/protocols | grep '	# ' | awk '{print $1}' | grep -vw -e 'ip' -e 'tcp' -e 'udp' -e 'icmp' -e 'esp' | grep -v 'ipv6' | sort | tr '\n' ' '"))
+  for p in string.gmatch(protocols, "%S+") do
+    field:value(p)
+  end
 end
 
 -- ------ rule configuration ------ --
@@ -46,46 +47,46 @@ ruleCheck()
 
 
 m5 = Map("mwan3", translate("MWAN Rule Configuration - ") .. arg[1],
-	translate(ruleWarn()))
-	m5.redirect = dsp.build_url("admin", "network", "mwan", "configuration", "rule")
+  translate(ruleWarn()))
+m5.redirect = dsp.build_url("admin", "network", "mwan", "configuration", "rule")
 
 
 mwan_rule = m5:section(NamedSection, arg[1], "rule", "")
-	mwan_rule.addremove = false
-	mwan_rule.dynamic = false
+mwan_rule.addremove = false
+mwan_rule.dynamic = false
 
 proto = mwan_rule:option(Value, "proto", translate("Protocol"),
-	translate("View the contents of /etc/protocols for protocol descriptions"))
-	proto.default = "all"
-	proto.rmempty = false
-	proto:value("all")
-	proto:value("ip")
-	proto:value("tcp")
-	proto:value("udp")
-	proto:value("icmp")
-	proto:value("esp")
-	cbiAddProtocol(proto)
+  translate("View the contents of /etc/protocols for protocol descriptions"))
+proto.default = "all"
+proto.rmempty = false
+proto:value("all")
+proto:value("ip")
+proto:value("tcp")
+proto:value("udp")
+proto:value("icmp")
+proto:value("esp")
+cbiAddProtocol(proto)
 
 src_ip = mwan_rule:option(Value, "src_ip", translate("Source address"),
-	translate("Supports CIDR notation (eg \"192.168.100.0/24\") without quotes"))
-	src_ip.datatype = ipaddr
+  translate("Supports CIDR notation (eg \"192.168.100.0/24\") without quotes"))
+src_ip.datatype = ipaddr
 
 src_port = mwan_rule:option(Value, "src_port", translate("Source port"),
-	translate("May be entered as a single or multiple port(s) (eg \"22\" or \"80,443\") or as a portrange (eg \"1024:2048\") without quotes"))
+  translate("May be entered as a single or multiple port(s) (eg \"22\" or \"80,443\") or as a portrange (eg \"1024:2048\") without quotes"))
 src_port:depends("proto", "tcp")
 src_port:depends("proto", "udp")
 
 dest_ip = mwan_rule:option(Value, "dest_ip", translate("Destination address"),
-	translate("Supports CIDR notation (eg \"192.168.100.0/24\") without quotes"))
-	dest_ip.datatype = ipaddr
+  translate("Supports CIDR notation (eg \"192.168.100.0/24\") without quotes"))
+dest_ip.datatype = ipaddr
 
 dest_port = mwan_rule:option(Value, "dest_port", translate("Destination port"),
-	translate("May be entered as a single or multiple port(s) (eg \"22\" or \"80,443\") or as a portrange (eg \"1024:2048\") without quotes"))
+  translate("May be entered as a single or multiple port(s) (eg \"22\" or \"80,443\") or as a portrange (eg \"1024:2048\") without quotes"))
 dest_port:depends("proto", "tcp")
 dest_port:depends("proto", "udp")
 
 dscp_class = mwan_rule:option(ListValue, "dscp_class", translate("DSCP Class match"),
-        translate("DSCP class"))
+  translate("DSCP class"))
 dscp_class.default = ""
 dscp_class:value("", translate("Default"))
 --dscp_class:value("cs0", translate("CS0 - Default"))
@@ -98,43 +99,43 @@ dscp_class:value("cs6", translate("CS6 - Network control"))
 --dscp_class:value("cs7", translate("CS7 - Reserved"))
 
 sticky = mwan_rule:option(ListValue, "sticky", translate("Sticky"),
-	translate("Traffic from the same source IP address that previously matched this rule within the sticky timeout period will use the same WAN interface"))
-	sticky.default = "0"
-	sticky:value("1", translate("Yes"))
-	sticky:value("0", translate("No"))
+  translate("Traffic from the same source IP address that previously matched this rule within the sticky timeout period will use the same WAN interface"))
+sticky.default = "0"
+sticky:value("1", translate("Yes"))
+sticky:value("0", translate("No"))
 
 timeout = mwan_rule:option(Value, "timeout", translate("Sticky timeout"),
-	translate("Seconds. Acceptable values: 1-1000000. Defaults to 600 if not set"))
-	timeout.datatype = "range(1, 1000000)"
+  translate("Seconds. Acceptable values: 1-1000000. Defaults to 600 if not set"))
+timeout.datatype = "range(1, 1000000)"
 
 ipset = mwan_rule:option(Value, "ipset", translate("IPset"),
-	translate("Name of IPset rule. Requires IPset rule in /etc/dnsmasq.conf (eg \"ipset=/youtube.com/youtube\")"))
+  translate("Name of IPset rule. Requires IPset rule in /etc/dnsmasq.conf (eg \"ipset=/youtube.com/youtube\")"))
 
 use_policy = mwan_rule:option(Value, "use_policy", translate("Policy assigned"))
-	cbiAddPolicy(use_policy)
-	use_policy:value("unreachable", translate("unreachable (reject)"))
-	use_policy:value("blackhole", translate("blackhole (drop)"))
-	use_policy:value("default", translate("default (use main routing table)"))
+cbiAddPolicy(use_policy)
+use_policy:value("unreachable", translate("unreachable (reject)"))
+use_policy:value("blackhole", translate("blackhole (drop)"))
+use_policy:value("default", translate("default (use main routing table)"))
 
 -- Autogenerated or modified rules tag
 gen = mwan_rule:option(DummyValue, "generated", translate("Generated rule"),
-        translate("This is an autogenerated rule"))
+  translate("This is an autogenerated rule"))
 gen:depends("generated", "1")
 
 edite = mwan_rule:option(Flag, "edited", translate("Keep settings"),
-        translate("Keep my customized values on network changes"))
+  translate("Keep my customized values on network changes"))
 edite.rawhtml = true
 function edite.cfgvalue(self, s)
-        return "1"
+  return "1"
 end
 edite:depends("generated", "1")
 
 -- ------ currently configured policies ------ --
 
 mwan_policy = m5:section(TypedSection, "policy", translate("Currently Configured Policies"))
-	mwan_policy.addremove = false
-	mwan_policy.dynamic = false
-	mwan_policy.sortable = false
-	mwan_policy.template = "cbi/tblsection"
+mwan_policy.addremove = false
+mwan_policy.dynamic = false
+mwan_policy.sortable = false
+mwan_policy.template = "cbi/tblsection"
 
 return m5
