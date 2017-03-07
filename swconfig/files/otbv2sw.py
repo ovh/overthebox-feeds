b9d97a6885a1cf5c117f696927330b9fa46bd985
Martin Wetterwald <martin.wetterwald@corp.ovh.com>
2017-03-07 15:23:52 +0100

    swconfig-otb: Auto isolate sw comments from output

--- swconfig/files/otbv2sw.py
+++ swconfig/files/otbv2sw.py
@@ -4,6 +4,7 @@
 
 import serial
 import config
+import re
 
 class State:
     def __init__(self, needle, goto_main_cmd):
@@ -42,33 +43,39 @@ class Sw(serial.Serial):
 
         self.open()
 
-    def _filter(self, line):
-        # TODO: Exclude also switch comments, not only empty lines
+    def recv(self, sent):
+        self.last_comments = []
+        # First, call self.readlines().
+        # It reads from serial port and gets a list with one line per list item.
+        # In each of the list's item, remove any \r or \n, but only at end of line (right strip)
+        # For each line of the output, give it to _filter_comments
+        # This will filter out comments and put them in a separated list
+        # Finally, use filter(None, list) remove empty elements
+        self.last_out = filter(None, [self._filter_comments(l.rstrip("\r\n")) for l in self.readlines()])
+        return (self.last_out, self.last_comments)
+
+    # Filter out all comments and push them in a separated list
+    def _filter_comments(self, line):
         # Comments example (always end by CRLF, but sometimes after prompt, sometimes on a new line)
         #   *Jan 13 2000 11:25:20: %System-5: New console connection for user admin, source async  ACCEPTED
         #   *Jan 13 2000 11:32:10: %Port-5: Port gi6 link down
         #   *Jan 13 2000 11:32:13: %Port-5: Port gi4 link up
-        # But keep them in a separate list so we could get information from them if needed
-        if not line:
-            return False
-        return True
 
-    def recv(self, sent):
-        # First, call self.readlines().
-        # It reads from serial port and gets a list with one line per list item.
-        # Then, exclude our echo, but only if it's found in the first line (index 0)
-        # In each of the list's item, remove any \r or \n, but only at end of line (right strip)
-        # Finally, use filter(_filter, list) to filter out empty elements and switch comments
-        # We end up with one nice filtered list, and the switch comments are separated in dedicated list
-        return filter(self._filter, [l.rstrip("\r\n") for i, l in enumerate(self.readlines())
-            if i != 0 or l.rstrip("\r\n") != sent.rstrip("\n")])
+        # This regex matches a switch comment
+        m = re.search(r'(\*.*: %.*: .*)', line)
+
+        # If we've found a comment, move it to a dedicated list
+        if m and m.group():
+            self.last_comments.append(m.group())
+            line = line[:m.span()[0]]
+
+        return line
 
     def _send(self, string, bypass_echo_check=False):
         # When sending a command, it's safer to send it char by char, and waiting for the echo
         # Why? Try to connect to the switch, go to the Username: prompt.
         # Then, in order to simulate high speed TX, copy "admin" and paste it inside the console
         # The echo arrives in a random order. The behaviour is completely unreliable
-        echo=""
         for char in string:
             self.write(char)
 
@@ -87,5 +94,12 @@ class Sw(serial.Serial):
 
         return self.recv(string)
 
+    # Send an arbitrary string to the switch and get the answer
+    # If bypass_echo_check is True, the echo will be part of the global answer
+    # Otherwise it'll be consumed char by char when checking for the command echo
+    def send_str(self, string, bypass_echo_check=False):
+        return self._send(string, bypass_echo_check)
+
+    # Send a command to the switch (\n is automatically appended)
     def send_cmd(self, cmd):
-        return self._send("%s\n" % (cmd))
+        return self.send_str("%s\n" % (cmd))
