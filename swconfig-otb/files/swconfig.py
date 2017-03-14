7c0ea8e812eb11df4f4ff1563eff473cbec8295d
Martin Wetterwald <martin.wetterwald@corp.ovh.com>
2017-03-14 18:15:29 +0100

    swconfig-otb: Implement parsing for uci network

--- swconfig-otb/files/swconfig.py
+++ swconfig-otb/files/swconfig.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 :
+"""swconfig CLI emulation
+
+This emulates the swconfig CLI, but instead of communicating with the switch using kernel driver,
+we connect to the switch via a serial connection.
+"""
 
 import sys
 import os
+
 from swconfig_otb.sw import Sw
+from swconfig_otb.uci import uci_to_dict
 from swconfig_otb.config import UCI_NAME, MODEL, CPU_PORT, PORTS, VLANS
+
+UCI_CONFIG_FILE = 'network'
 
 def _usage():
     sys.exit("%s dev <dev> (help|load <config>|show)" % os.path.basename(sys.argv[0]))
 
 def _help():
     print(
-        "{sw_name}: {sw_name}({sw_type}), ports: {sw_ports} (cpu @ {sw_cpu_port}), vlans: {sw_vlans}"
-    ).format(sw_name=UCI_NAME, sw_type=MODEL, sw_ports=PORTS, sw_cpu_port=CPU_PORT, sw_vlans=VLANS)
+        "{name}: {name}({type_}), ports: {ports} (cpu @ {cpu_port}), vlans: {vlans}"
+    ).format(name=UCI_NAME, type_=MODEL, ports=PORTS, cpu_port=CPU_PORT, vlans=VLANS)
 
 def _show():
     pass
 
 def _load(args):
+    if len(args) != 2:
+        _usage()
+
+    if args[1] != UCI_CONFIG_FILE:
+        sys.exit("Sorry, only '%s' is supported for the config file" % (UCI_CONFIG_FILE))
+
     with Sw() as switch:
+        uci_config = uci_to_dict(UCI_CONFIG_FILE)
         vlans_current = switch.parse_vlans()
 
 def _cli():
@@ -40,7 +56,6 @@ def _cli():
         _show()
     else:
         _usage()
-
 
 if __name__ == '__main__':
     _cli()
