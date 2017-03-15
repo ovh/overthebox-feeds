5c4d05a3e8a34ba66543c80832a1960eb5a986ff
Martin Wetterwald <martin.wetterwald@corp.ovh.com>
2017-03-15 16:05:43 +0100

    swconfig-otb: Convert UCI VLAN conf to VLAN dict

--- swconfig-otb/files/swconfig.py
+++ swconfig-otb/files/swconfig.py
@@ -34,9 +34,64 @@ def _load(args):
     if args[1] != UCI_CONFIG_FILE:
         sys.exit("Sorry, only '%s' is supported for the config file" % (UCI_CONFIG_FILE))
 
+    uci_config = uci_to_dict(UCI_CONFIG_FILE)
+    vlans_wanted = _uci_dict_to_vlan_dict(uci_config)
+
     with Sw() as switch:
-        uci_config = uci_to_dict(UCI_CONFIG_FILE)
         vlans_current = switch.parse_vlans()
+
+
+def _uci_dict_to_vlan_dict(uci_dict):
+    if 'switch' not in uci_dict:
+        sys.exit("No 'switch' section found in the UCI config")
+
+    switches = uci_dict['switch']
+    if not any('name' in d and d['name'] == UCI_NAME for _, d in enumerate(switches)):
+        sys.exit("No 'switch' section contained a 'name' key with '%s' in UCI config" % (UCI_NAME))
+
+    vlan_dict = {}
+
+    if 'switch_vlan' not in uci_dict:
+        return vlan_dict
+
+    vlans = uci_dict['switch_vlan']
+
+    # Browse each 'switch_vlan' section which has a 'device' key with value UCI_NAME
+    uci_vlans = (v for i, v in enumerate(vlans) if 'device' in v and v['device'] == UCI_NAME)
+    for uci_vlan in uci_vlans:
+        # Skip the switch_vlan section if the keys we care about are missing
+        if not ('vlan' in uci_vlan and 'ports' in uci_vlan):
+            continue
+
+        try:
+            vid, ports = int(uci_vlan['vlan']), uci_vlan['ports'].split()
+        except ValueError:
+            # Skip this VLAN if we don't understand it (it's not a number)
+            print "Skipping strange VID '%s'" % (vid)
+            continue
+
+        if vid in vlan_dict:
+            print "Skipping duplicate VID %d declaration" % (vid)
+            continue
+
+        vlan_dict[vid] = {}
+
+        for port in ports:
+            tagged = False
+
+            if port[-1] == 't':
+                port = port[:-1]
+                tagged = True
+
+            # Add the port to the VLAN in our vlan_dict
+            try:
+                vlan_dict[vid][int(port)] = tagged
+            except ValueError:
+                # Skip this port if we don't understand it (it's not a number)
+                print "Skipping strange port '%s'" % (port)
+                continue
+
+    return vlan_dict
 
 def _cli():
     if len(sys.argv) < 4:
