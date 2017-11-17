7de2ddd161abdb34a1f4f4e7fcd122b31e53b50f
Grégoire Delattre <gregoire.delattre@gmail.com>
2017-11-17 20:54:38 +0100

    luci-app-overthebox: Remove useless lines

--- luci-app-overthebox/luasrc/model/cbi/dscp/dscp.lua
+++ luci-app-overthebox/luasrc/model/cbi/dscp/dscp.lua
@@ -7,8 +7,6 @@ local wa = require "luci.tools.webadmin"
 local ut = require "luci.util"
 local sys = require "luci.sys"
 
-local mud = (uci:get("glorytun", "tun0") == "mud" and uci:get("glorytun", "tun0", "enable") == "1") or (uci:get("glorytun", "xtun0") == "mud" and uci:get("glorytun", "xtun0", "enable") == "1")
-
 m = Map("dscp", translate("Differentiated services"),
 	translate("Traffic may be classified by many different parameters, such as source address, destination address or traffic type and assigned to a specific traffic class."))
 
@@ -29,18 +27,10 @@ direction = s:option(ListValue, "direction", translate("Direction"))
 	direction.default = "upload"
 	direction.rmempty = false
 	direction:value("upload")
-if mud then
 	direction:value("download")
 	direction:value("both")
-end
-
---dpi = s:option(Value, "dpi", translate("Service"))
---dpi.rmempty = true
---dpi:depends("direction", "download")
---dpi:value("", translate("Disabled"))
 
 proto = s:option(Value, "proto", translate("Protocol"))
-	proto:depends("dpi", "")
 	proto.default = "all"
 	proto.rmempty = false
 	proto:value("tcp")
@@ -52,7 +42,6 @@ proto = s:option(Value, "proto", translate("Protocol"))
 	cbiAddProtocol(proto)
 
 srch = s:option(Value, "src_ip", translate("Source host"))
-	srch:depends("dpi", "")
 	srch.rmempty = true
 	srch:value("", translate("all"))
 	wa.cbi_add_knownips(srch)
@@ -66,21 +55,14 @@ sports = s:option(Value, "src_port", translate("Source ports"))
 dsth = s:option(Value, "dest_ip", translate("Destination host"))
 	dsth.rmempty = true
 	dsth:value("", translate("all"))
-if mud then
 	dsth:depends("direction", "upload")
-end
 	wa.cbi_add_knownips(dsth)
 
 dports = s:option(Value, "dest_port", translate("Destination ports"))
 	dports.rmempty = true
 	dports:value("", translate("all"))
-if mud then
 	dports:depends({proto="tcp", direction="upload"})
 	dports:depends({proto="udp", direction="upload"})
-else
-	dports:depends("proto","tcp")
-	dports:depends("proto","udp")
-end
 
 t = s:option(ListValue, "class", translate("Class"))
 	t:value("cs1", translate("CS1 - Scavenger"))
@@ -90,10 +72,8 @@ t = s:option(ListValue, "class", translate("Class"))
 	t:value("cs5", translate("CS5 - Broadcast video"))
 	t:value("cs6", translate("CS6 - Network control"))
 	t:value("cs7", translate("CS7 - Reserved"))
-	--t:value("drop", translate("Drop"))
 	t.default = "cs2"
 
 comment = s:option(Value, "comment", translate("Comment"))
---bytes = s:option(Value, "connbytes", translate("Number of bytes"))
 
 return m
