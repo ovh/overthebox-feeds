cf2e7922d28fca12616928b2e2fd7b7f0c35ffbb
Adrien Gallouët <adrien@gallouet.fr>
2018-06-21 10:43:44 +0000

    otb-luci: Add support for linking a device with a service
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- otb-luci/files/usr/lib/lua/luci/controller/overthebox.lua
+++ otb-luci/files/usr/lib/lua/luci/controller/overthebox.lua
@@ -5,19 +5,20 @@ function index()
   entry({"admin", "overthebox"}, alias("admin", "overthebox", "overview"), "OverTheBox", 10).index = true
   entry({"admin", "overthebox", "overview"}, template("otb_overview"), "Overview", 1)
   entry({"admin", "overthebox", "qos"}, cbi("otb_qos"), "QoS", 2)
-  entry({"admin", "overthebox", "activate"}, call("otb_activate")).dependent = false
+  entry({"admin", "overthebox", "confirm_service"}, call("otb_confirm_service")).dependent = false
+  entry({"admin", "overthebox", "time"}, call("otb_time")).dependent = false
 end
 
-function otb_activate()
-  local dump = io.popen("otb-confirm-service")
-  if dump then
-    for line in dump:lines() do
-      if line == "OK" then
-        luci.http.status(200, "OK")
-        dump:close()
-        return
-      end
-    end
+function otb_confirm_service()
+  local service = luci.http.formvalue("service") or ""
+  if os.execute("otb-confirm-service "..service) then
+    luci.http.status(200, "OK")
+  else
+    luci.http.status(500, "ERROR")
   end
-  luci.http.status(500, "Oups")
+end
+
+function otb_time()
+  luci.http.prepare_content("application/json")
+  luci.http.write_json({ timestamp = tostring(os.time()) })
 end
